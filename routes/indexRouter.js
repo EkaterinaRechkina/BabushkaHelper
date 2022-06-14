9723b4d89693782bb2f48f1b5461eebfe004eae9
Evtifeva Elena <lenaev84@gmail.com>
2022-06-14 15:49:44 +0300

    registr

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
 // const { Post } = require("../db/models");
 
+
+
 const router = Router();
 
 
-
+router.get('/register', (req, res) => {
+  res.render('users/registration');
+})
 
 
 
