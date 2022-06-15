1c1aaac3f5c67266ad6614c93c0333cfd491be18
Evtifeva Elena <lenaev84@gmail.com>
2022-06-15 16:43:16 +0300

    login,registrWork

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,20 @@
-// const { postForm } = document.forms;
-// const container = document.querySelector("#container");
-console.log('helllooow');
-
-
+console.log('helllooow LOGIIIN');
 
 const form = document.querySelector('form');
 
-console.log(form, "form");
+// console.log(form, "form");
 form.addEventListener('submit', async (event) => {
   // * Если event.preventDefault() - то форма не отправляется,
   // * стр. не перезагружается, запись в БД не создаётся
   event.preventDefault();
   
   const granny_name = event.target.username.value;//по назв инпута
+  console.log(granny_name, '===========12');
   const password = event.target.password.value;
-  // console.log('значения', username, password);
   const bodyObj = { granny_name, password};
   console.log(bodyObj);
 
-
-  const response = await fetch('/registration', {//!адрес с action из формы\
+  const response = await fetch('/login', {//!адрес с action из формы\
     
     method: 'POST',
     credentials: 'include',
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,21 +1,81 @@
 const { Router } = require("express");
-// const { Post } = require("../db/models");
-
+const {  Granny, Grandchild} = require("../db/models");
+const { checkIsSession, checkIsNotSession } = require('../middlewares/index.middlewares')
 const router = Router();
 
+//!для хэширования паролей:
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 router.get('/', (req, res) => {
   res.render('users/login');
 })
 
+router.post('/', async(req, res) => {
+  const { granny_name, password} = req.body
+console.log(req.body);
+  try {
+   const user = await Granny.findOne({
+        where: {
+          granny_name,
+        },
+      })
+  console.log(user)
+  
+  //сначала проверяем по логину в базе
+      if(!user) {
+        // throw Error('такого логина нет...')
+        return res.render('error', {
+          message: 'упс...такого логина не существует...(((',
+          error: {}
+        });
+         }
+   
+  
+  const isValidPass = await bcrypt.compare(password, user.password)//расхэшируем пароль
+  
+   //затем проверяем пароль 
+  if(!isValidPass) {
+    // throw Error('неправильный пароль...')
+    return res.render('error', { //TODO return обязателен везде!
+      message: '!!!неправильный пароль...попробуйте еще',
+      error: {}
+    });
+    // res.render('/error')
+  
+    //если прошли роверки на логин и пароль, создаем сессию
+  } else {
+    req.session.granny_name = user.granny_name // добавляем в сессию айди нового юзера
+    // // console.log(reg.session);
+   
+  
+    res.redirect('/')
+    
+  }
+   
 
-router.post('/', (req, res) => {
 
 
-  res.render('./grannyMain')
+} catch (error) {
+    console.error(error)
+  }
 })
 
+//!убить куки и сессию
+router.get('/logout', checkIsNotSession, (req, res) => {
+  req.session.destroy();
+  res.clearCookie(process.env.COOKIE_NAME)
+  
+  res.render('/')
+})
+
+
+// router.post('/', (req, res) => {
+
+
+//   res.render('./grannyMain')
+// })
 
 
 module.exports = router;
