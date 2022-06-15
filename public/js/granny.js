000f1f483132b1a139aeffeebe8f238877171b60
KuvaldinDmitry <kuvaldin2012@gmail.com>
2022-06-15 17:08:40 +0400

    eveningPush

--- public/js/granny.js
+++ public/js/granny.js
@@ -1,6 +1,8 @@
 //Распознаем изображение
 // import Tesseract from "tesseract.js";
 
+
+
 const log = document.getElementById("log");
 const pre = document.getElementsByClassName("pre");
 function recognize(file, lang, logger) {
