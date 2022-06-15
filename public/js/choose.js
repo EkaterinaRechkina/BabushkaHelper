b625d9f211751512fb8029a5ab369ac8aeec3afc
EkaterinaRechkina <ekaterinarechkinav@yandex.ru>
2022-06-15 21:57:27 +0300

    0.3

--- public/js/choose.js
+++ public/js/choose.js
@@ -1,12 +1,12 @@
-const granny = document.getElementById('granny')
-const child = document.getElementById('child')
-const grannyForm = document.querySelector('.grannyForm')
-const choose = document.querySelector('.choose')
+const granny = document.getElementById("granny");
+const child = document.getElementById("child");
+const grannyForm = document.querySelector(".grannyForm");
+const choose = document.querySelector(".choose");
 
 console.log(granny);
-granny.addEventListener('click', (event) => {
-event.preventDefault();
-grannyForm.style.display = "block"
-choose.style.display = "none"
-console.log('adddddddd')
-})
+granny.addEventListener("click", (event) => {
+  event.preventDefault();
+  grannyForm.style.display = "block";
+  choose.style.display = "none";
+  console.log("adddddddd");
+});
