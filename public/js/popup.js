0fb3bb7b33bfe2888762856f290d2742e9c14736
EkaterinaRechkina <ekaterinarechkinav@yandex.ru>
2022-06-15 22:28:58 +0300

    add popup

---
+++ public/js/popup.js
@@ -0,0 +1,16 @@
+// const popup = document.querySelector(".modal");
+// console.log(popup);
+// const img = document.getElementById("myImg");
+// const popupImg = document.getElementById("img01");
+// const captionText = document.getElementById("caption");
+// img.onclick = function () {
+//   popup.style.display = "block";
+//   popupImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// };
+
+// const span = document.getElementsByClassName("close")[0];
+
+// span.onclick = function () {
+//   popup.style.display = "none";
+// };
