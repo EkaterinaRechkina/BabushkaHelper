6ffe7e5093559dc9065f11097a672c36655dff33
EkaterinaRechkina <73222009+EkaterinaRechkina@users.noreply.github.com>
2022-08-02 11:20:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
 
-// const indexRouter = require("./routes/indexRouter");
+
 const grannyRouter = require("./routes/granny.routes");
-// const postsRouter = require('./routes/posts.routes')
 const registrationRouter = require("./routes/registrationRouter.js");
 const loginRouter = require("./routes/loginRouter.js");
 const allPostsRouter = require("./routes/allPostsRouter");
@@ -26,34 +25,30 @@ hbs.registerHelper("each_upto", function (ary, max, options) {
 
 hbs.registerPartials(path.join(process.env.PWD, "views", "partials"));
 
-//! шаг 1настройка сессии
+
 const session = require("express-session"); //кука
 const FileStore = require("session-file-store")(session); //хранение сессий
 
 app.set("view engine", "hbs");
-// app.set("views", path.join(process.env.PWD, "views"));
 
 hbs.registerPartials(`${__dirname}/views/partials`);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//! шаг 2 настройки сессии
+
 const sessionConfig = {
-  store: new FileStore(), // хранилище сессий
-  name: process.env.COOKIE_NAME, // ключ куки(енв)
-  secret: process.env.SECRET, // шифрование id сессии(в данном случае поменяли на енв )
-  resave: false, // пересохранение сессии (когда что-то поменяли - false)
-  saveUninitialized: false, // сохраняем пустую сессию (чтоб посмотреть)
-  httpOnly: true, // нельзя изменить куки с фронта
+  store: new FileStore(), 
+  name: process.env.COOKIE_NAME, 
+  secret: process.env.SECRET,
+  resave: false, 
+  saveUninitialized: false, 
+  httpOnly: true, 
   cookie: { expires: 24 * 60 * 60e3 },
 };
 
 app.use(session(sessionConfig));
-//---------------------------------------------------------------
-//TODO ПОРЯДОК ОЧЕНЬ ВАЖЕН!!! вот этого что снизу!
-//!мидлвара, которая применятся к каждому обработчику, проверка-есть ли сессия
-//!тут сохраняем данные из сессии для использования в наших ХБСках
+
 app.use((req, res, next) => {
   if (req.session.name) {
     res.locals.granny_name = req.session.granny_name;
